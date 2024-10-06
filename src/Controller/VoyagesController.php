a7b1561060e320417142c3b593ae9457cba0a512
STL <S_T_L@laposte.net>
2024-10-06 17:13:31 +0200

    Phase 9 : ORM - affichage détaillé d'une visite

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -61,6 +61,15 @@ class VoyagesController extends AbstractController{
         ]);
     } 
     
+    #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [
+            'visite' => $visite
+        ]);
+    }
+    
+    
     
     
 }
