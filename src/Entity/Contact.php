25db619d4cd759b91720ff833eb4d254d0d26448
STL <S_T_L@laposte.net>
2024-10-21 15:31:01 +0200

    Phase 18 : Gestion des mails

---
+++ src/Entity/Contact.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Asserts;
+
+/**
+ * Description of Contact
+ *
+ * @author estel
+ */
+class Contact {
+   
+    #[Assert\NoBlank()]
+    #[Assert\Lenght(min:2, max:100)]
+    private ?string $nom;
+    
+    #[Assert\NoBlank()]
+    #[Assert\Email()]
+    private ?string $email;
+    
+    #[Assert\NoBlank()]
+    private ?string $message;
+    public function getNom(): ?string {
+        return $this->nom;
+    }
+
+    public function getEmail(): ?string {
+        return $this->email;
+    }
+
+    public function getMessage(): ?string {
+        return $this->message;
+    }
+
+    public function setNom(?string $nom) {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function setEmail(?string $email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setMessage(?string $message) {
+        $this->message = $message;
+        return $this;
+    }
+
+
+}
