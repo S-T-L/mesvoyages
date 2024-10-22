3ead8f1dd0880fb26647b00287bad8406de86720
STL <S_T_L@laposte.net>
2024-10-22 17:36:40 +0200

    Phase 22 : Tests Complémentaires

--- tests/Validations/VisiteValidationsTest.php
+++ tests/Validations/VisiteValidationsTest.php
@@ -17,39 +17,55 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
  * @author estel
  */
 class VisiteValidationsTest extends KernelTestCase {
-   public function getVisite() : Visite{
-       return (new Visite())
-                ->setVille("New York")
-                ->setPays("USA");
-   }
-   
-   public function testValidNoteVisite(){
-       $visite = $this->getVisite()->setNote(10);
-       self::bootKernel();
-       $this->assertErrors($visite, 0);
-       
-   }
-   
-   public function assertErrors(Visite $visite, int $nbErreursAttendues, string $message=""){
-       self::bootKernel();
-       $validator = self::getContainer()->get(ValidatorInterface::class);
-       $error=$validator->validate($visite);
-       $this->assertCount($nbErreursAttendues, $error, $message);
-       
-   }
-   
-   public function testNonValidNoteVisite(){
-       $visite = $this->getVisite()->setNote(21);
-       self::bootKernel();
-       $this->assertErrors($visite, 1);
-       
-   }
-   
-   public function testNonValidtempmaxVisite(){
-       $visite = $this->getVisite()
-               ->setTempmin(20)
-               ->setTempmax(18);
-        $this->assertErrors($visite, 1, "min=20, max=18 devrait échouer");
-                
-   }
+
+    public function getVisite(): Visite {
+        return (new Visite())
+                        ->setVille("New York")
+                        ->setPays("USA");
+    }
+
+    public function testValidNoteVisite() {
+        $this->assertErrors($this->getVisite()->setNote(10), 0, "10 devrait réussir");
+        $this->assertErrors($this->getVisite()->setNote(0), 0, "0 devrait réussir");
+        $this->assertErrors($this->getVisite()->setNote(20), 0, "20 devrait réussir");
+    }
+
+    public function assertErrors(Visite $visite, int $nbErreursAttendues, string $message = "") {
+        self::bootKernel();
+        $validator = self::getContainer()->get(ValidatorInterface::class);
+        $error = $validator->validate($visite);
+        $this->assertCount($nbErreursAttendues, $error, $message);
+    }
+
+    public function testNonValidNoteVisite() {
+        $this->assertErrors($this->getVisite()->setNote(21), 1, "21 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(-1), 1, "-1 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(25), 1, "25 devrait échouer");
+        $this->assertErrors($this->getVisite()->setNote(-10), 1, "-10 devrait échouer");
+    }
+
+    public function testNonValidtempmaxVisite() {
+        $this->assertErrors($this->getVisite()->setTempmin(15)->setTempmax(2), 1, "15min et 2max devrait échouer");
+        $this->assertErrors($this->getVisite()->setTempmin(15)->setTempmax(15), 1, "15min et 15max devrait échouer");
+    }
+
+    public function testValidtempmaxVisite() {
+        $this->assertErrors($this->getVisite()->setTempmin(5)->setTempmax(10), 0, "5 et 10 devrait réussir");
+        $this->assertErrors($this->getVisite()->setTempmin(10)->setTempmax(11), 0, "10 et 11 devrait réussir");
+    }
+
+    public function testValidDatecreationVisite() {
+        $aujourdhui = new \DateTime();
+        $this->assertErrors($this->getVisite()->setDatecreation($aujourdhui), 0, "aujourd'hui devrait réussir");
+        //Soustrait 5 jours à la date actuelle, en utilisant l'intervalle de temps p5d
+        $plustot = (new \DateTime())->sub(new \DateInterval("P5D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($plustot), 0, "plus tôt devrait réussir");
+    }
+
+    public function testNonValidDatecreationVisite() {
+        $demain = (new \DateTime())->add(new \DateInterval("P1D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($demain), 1, "demain devrait échouer");
+        $plustard = (new \DateTime())->add(new \DateInterval("P5D"));
+        $this->assertErrors($this->getVisite()->setDatecreation($plustard), 1, "plus tôt devrait échouer");
+    }
 }
--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -7,7 +7,9 @@
 
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -18,7 +20,18 @@ use PHPUnit\Framework\TestCase;
 class VisiteTest extends TestCase{
     public function testGetDatecreationString(){
         $visite= new Visite();
-        $visite->setDatecreation(new \DateTime("2024/04/24"));
+        $visite->setDatecreation(new DateTime("2024/04/24"));
         $this->assertEquals("24/04/2024", $visite->getDateCreationString());
+    }
+    
+         public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
     }
 }
